931695a898d7432a6d675dd06a749bff111971d9
Noh <noelen_g@outlook.com>
2023-10-29 13:06:04 -0300

    config paths in vite and tsconfig.

--- vite.config.ts
+++ vite.config.ts
@@ -1,14 +1,8 @@
 import react from '@vitejs/plugin-react';
-import { fileURLToPath } from 'node:url';
 import { defineConfig } from 'vite';
+import tsconfigPaths from 'vite-tsconfig-paths';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
-  resolve: {
-    alias: {
-      find: '@',
-      replacement: fileURLToPath(new URL('./src', import.meta.url)),
-    },
-  },
+  plugins: [react(), tsconfigPaths()],
 });
